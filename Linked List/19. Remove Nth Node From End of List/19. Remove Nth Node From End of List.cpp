ec5e3bda046782c920b31dc3ab1c740fbf0751cf
pzh <1972508489@qq.com>
2019-05-25 17:57:13 +0800

    Update 19. Remove Nth Node From End of List.cpp

--- Linked List/19. Remove Nth Node From End of List/19. Remove Nth Node From End of List.cpp
+++ Linked List/19. Remove Nth Node From End of List/19. Remove Nth Node From End of List.cpp
@@ -6,44 +6,38 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+// 非常基础的链表问题，同样使用双指针法进行求解: 
+// (1) 创建一个快指针fast和慢指针slow指向链表头结点head
+// (2) 将快指针向前移动k步
+// (3) 同时循环移动快指针和慢指针一步，直到快指针指向NULL，这时慢指针恰好指向链表的倒数第k个结点，删除慢指针这时所指向的结点即可
+
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode *fast=head;      // 快指针
+        ListNode *slow=head;      // 慢指针
+        ListNode *slowprev=NULL;  // 慢指针的前驱结点，因为要删除慢指针所指向的结点，因此需要一个额外的指针指向慢指针的前驱结点
 
-        // 1. 基本算法实现
-        // 处理删除链表倒数第n个结点基本算法是快慢指针法：创建两个初始时指向头结点的指针fast和slow，然后将fast先移动n步，接着slow和fast一直向前移动，直到fast->next==NULL停止，这时slow就指向待删除结点的前向结点
-        // 警告: 上述处理一般情况的算法并不适用于处理要删除的结点为头结点的情况，fast移动n步时，n<=m-1，其中m为链表结点的个数，否则后续无法移动fast满足fast->next==NULL
-        
-        ListNode *fast=head;    //快指针
-        ListNode *slow=head;    //慢指针
-        
-        // 1.1 移动快指针n步
         for(int i=0;i<n;i++)
         {
             fast=fast->next;
         }
 
-        // 1.2 判断快指针移动n步后是否为空结点，若移动n步后，快指针移动到了空结点，则说明链表的结点总数为n，因此这时要删除的倒数第n个结点就是头结点，删除头结点需要进行特殊处理
-        if(fast==NULL)
+        while(fast)
         {
-            ListNode *temp=head;
-            head=head->next;      //头结点的后继结点作为新的头结点
-            delete temp;          //删除头结点
-            return head;
+            slowprev=slow;
+            fast=fast->next;
+            slow=slow->next;
         }
 
-        // 1.3 同时移动快指针和慢指针，直到快指针满足fast->next==NULL，这时慢指针指向待删除结点的前驱结点
+        if(slowprev)
+        slowprev->next=slow->next;
         else
-        {
-            while(fast->next!=NULL)
-            {
-                fast=fast->next;
-                slow=slow->next;
-            }
-            ListNode *temp=slow->next;
-            slow->next=slow->next->next;    //使用两次->next需要极度小心
-            delete temp;
-            return head;
-        }
+        head=head->next;
+        
+        delete slow;  
+        
+        return head;
     }
 };
